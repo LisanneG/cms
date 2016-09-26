d22a461a880976637be66071358c94f07a621ff2
mennogodeke <mgodeke1994@gmail.com>
2016-09-26 15:21:39 +0200

    added images to locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923092742) do
+ActiveRecord::Schema.define(version: 20160926123903) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160923092742) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
+    t.string   "image"
   end
 
   create_table "questions", force: :cascade do |t|
